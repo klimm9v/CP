35e9595a60d7fb37bde65a3f4a3d365cb8ee49a9
Linus <khabibtime@rambler.ru>
2024-09-14 21:01:13 +0300

    arr, input

---
+++ utils/arr_utils.py
@@ -0,0 +1,8 @@
+def array_int():
+    return [int(i) for i in input().split()]
+
+def array_float():
+    return [float(i) for i in input().split()]
+
+def array_str():
+    return [float(i) for i in input().split()]
---
+++ utils/input_utils.py
@@ -0,0 +1,13 @@
+from collections import Counter
+
+def u28() -> int:
+    return int(input())
+
+def i24() -> float:
+    return float(input())
+
+def strin() -> str:
+    return input()
+
+def counts(s) -> dict:
+    return Counter(s)
